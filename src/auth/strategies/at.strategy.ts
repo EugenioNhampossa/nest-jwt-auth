e393922ff241aed484cc291494f1f222c9fa126b
EugenioNhampossa <eugenioanhampossa@gmail.com>
2023-03-13 21:38:56 +0200

    chore: add strategies

---
+++ src/auth/strategies/at.strategy.ts
@@ -0,0 +1,18 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AtStrategy extends PassportStrategy(Strategy, 'jwt-access') {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: 'access-secret-key',
+      passReqToCallBack: true,
+    });
+  }
+
+  validate(payload: any) {
+    return payload;
+  }
+}
