5146d5acecd190dd19ba8c51c8a13c3268eba3a7
EugenioNhampossa <eugenioanhampossa@gmail.com>
2023-03-18 16:43:17 +0200

    feat: roles based authorisation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { APP_GUARD } from '@nestjs/core';
 import { AuthModule } from './auth/auth.module';
-import { RoleGuard } from './common/guards';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 
@@ -15,12 +13,5 @@ import { UserModule } from './user/user.module';
     }),
     UserModule,
   ],
-
-  // providers: [
-  //   {
-  //     provide: APP_GUARD,
-  //     useClass: RoleGuard,
-  //   },
-  // ],
 })
 export class AppModule {}
